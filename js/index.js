f7fa762e678243f9c12cab59f24cb1e0d686cc30
Ana Paula <anapdebner21@gmail.com>
2021-11-13 03:24:06 -0300

    aposentadoria, palavras, imc!!

--- js/index.js
+++ js/index.js
@@ -3,9 +3,50 @@ function imc(){
     let altura = prompt ("Informe sua altura:");
     let peso = prompt ("Informe seu peso:");
     let imc = peso/(altura * altura);
+console.log(imc);
+  
+}
 
-    alert(`Olá ${nome} ! Seu  Índice de Massa Corporal (IMC) é de  ${imc} ! Se resultou em 25 ou mais, entre em contato com a gente! Queremos  tornar sua vida mais leve =)`);
+function aposentadoria(){
+   const nome = prompt('Qual é o seu nome?');
+   let idade=calcularIdade(prompt('Que ano você nasceu?'));
+   if(idade){
+    alert(`Parabéns ${nome} você conseguiu esse grande feito, você irá se aposentar!`);
+   }else{
+       alert(`IH tem que trabalhar mais um pouco ${nome}`)
+        let falta = 65 - checa; 
+        console.log(falta);
+        alert (`Faltam ${falta} anos pra vc se aposentar!!`);
+        console.log(idade); 
+    }
+
+}
+function calcularIdade(anoNascimento){
+    checa = 2021 - anoNascimento ;
+    let aposenta = true;
+    console.log(checa);
+    checa < 65 ? aposenta = false : aposenta = true; 
+    return aposenta; 
+}
+
+function numero(){
+    var palavras = new Array();
+    var n = prompt('Quantas palavras vc quer colocar?');
+    console.log(palavras);
+    for(let i = 0; i < n; i++){
+        palavras[i] = prompt('Informe uma palavra:');
+
     
+    }
+    palavras.reverse();
+    for(let i = 0; i < n; i++){
+        alert(palavras[i]);
+        console.log(palavras[i]);
+    }
+
+    
+    
+
 }
 
 
